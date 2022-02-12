327d189948a4653766eecb1c5490995f3cddac40
EmirSavukV2 <emirsvk55@gmail.com>
2022-02-12 18:06:41 +0300

    t
    t

--- lib/views/screens/auth/auth.view.dart
+++ lib/views/screens/auth/auth.view.dart
@@ -11,88 +11,257 @@ class AuthScreen extends SFW {
 
   AuthController get getController => controller as AuthController;
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  openVerificationSMS() {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        useRootNavigator: true,
+        context: state.context,
+        builder: (context) {
+          return Padding(
+            padding: MediaQuery.of(context).viewInsets,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.phone,
+                    maxLength: 6,
+                    decoration: const InputDecoration(
+                      label: const Text("Doğrulama Kodu"),
+                    ),
+                    controller: getController.formController["code"],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      getController.user
+                          .confirmPhoneCode(
+                              getController.formController["code"]!.text)
+                          .then((value) => value != null
+                              ? getController.loginSuccess(value)
+                              : "");
+                    },
+                    child: const Text("Doğrula"),
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 18)),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build() {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       key: scaffoldKey,
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.abc_rounded), label: "s"),
-          BottomNavigationBarItem(icon: Icon(Icons.abc), label: "c"),
-        ],
-      ),
       body: Container(
-        decoration: BoxDecoration(),
-        height: MediaQuery.of(state.context).size.height,
-        width: MediaQuery.of(state.context).size.width,
-        alignment: Alignment.center,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-            child: Form(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage('https://picsum.photos/seed/329/900'),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://storage.googleapis.com/flutterflow-io-6f20.appspot.com/flutterflow_assets/ff_logo.png',
+                  width: 100,
+                  height: 100,
+                )
+              ],
+            ),
+            Form(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Container(
-                  //   margin: EdgeInsets.only(bottom: 80),
-                  //   child: Image.network(),
-                  // ),
-                  Card(
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 0, horizontal: 20),
-                            child: IntlPhoneField(
-                              controller: getController.formController["phone"],
-                              decoration: InputDecoration(
-                                labelText: 'Telefon Numarası',
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(),
-                                ),
-                              ),
-                              initialCountryCode: 'TR',
-                              onChanged: (phone) {
-                                getController.phoneNumber =
-                                    phone.completeNumber;
-                              },
-                              onCountryChanged: (country) {
-                                print('Country changed to: ' + country.name);
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
                   Container(
-                    margin: EdgeInsets.only(top: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    child: Column(
                       children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 30),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 20),
+                          child: IntlPhoneField(
+                            dropdownIcon: Icon(
+                              Icons.arrow_drop_down,
+                              color: Colors.white,
+                            ),
+                            cursorColor: Colors.white,
+                            controller: getController.formController["phone"],
+                            decoration: InputDecoration(
+                              hintStyle: const TextStyle(color: Colors.white),
+                              fillColor: Color.fromRGBO(255, 255, 255, 0.4),
+                              labelText: 'Telefon Numarası',
+                              labelStyle: const TextStyle(color: Colors.white),
+                              border: const UnderlineInputBorder(
+                                borderSide: BorderSide(),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: const BorderSide(
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: const BorderSide(
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              filled: true,
+                            ),
+                            style: const TextStyle(
+                                color: Colors.white,
+                                decorationColor: Colors.white),
+                            initialCountryCode: 'TR',
+                            onChanged: (phone) {
+                              getController.phoneNumber = phone.completeNumber;
+                            },
+                            onCountryChanged: (country) {
+                              print('Country changed to: ' + country.name);
+                            },
                           ),
-                          onPressed: () {
-                            getController.onClickLogin();
-                          },
-                          child: Text("Telefonu Doğrula"),
                         ),
                       ],
                     ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 40,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            side: BorderSide(
+                              color: Color(0xff000000),
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          elevation: 1,
+                        ),
+                        onPressed: () {
+                          getController.onClickLogin();
+                        },
+                        child: const Text("Telefonu Doğrula"),
+                      ),
+                    ],
                   )
                 ],
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
 }
+
+// class AuthScreen2 extends StatefulWidget {
+//   const AuthScreen2({Key? key}) : super(key: key);
+
+//   @override
+//   _AuthScreen2State createState() => _AuthScreen2State();
+// }
+
+// class _AuthScreen2State extends State<AuthScreen2> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: double.infinity,
+//       height: double.infinity,
+//       decoration: BoxDecoration(
+//         image: DecorationImage(
+//             image: NetworkImage('https://picsum.photos/seed/329/900'),
+//             fit: BoxFit.cover),
+//       ),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Image.network(
+//                 'https://storage.googleapis.com/flutterflow-io-6f20.appspot.com/flutterflow_assets/ff_logo.png',
+//                 width: 100,
+//                 height: 100,
+//               )
+//             ],
+//           ),
+//           Form(
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 // Container(
+//                 //   margin: EdgeInsets.only(bottom: 80),
+//                 //   child: Image.network(),
+//                 // ),
+//                 Container(
+//                   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+//                   child: Column(
+//                     children: [
+//                       Container(
+//                         padding:
+//                             EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+//                         child: IntlPhoneField(
+//                           controller: getController.formController["phone"],
+//                           decoration: InputDecoration(
+//                             labelText: 'Telefon Numarası',
+//                             border: UnderlineInputBorder(
+//                               borderSide: BorderSide(),
+//                             ),
+//                           ),
+//                           initialCountryCode: 'TR',
+//                           onChanged: (phone) {
+//                             getController.phoneNumber = phone.completeNumber;
+//                           },
+//                           onCountryChanged: (country) {
+//                             print('Country changed to: ' + country.name);
+//                           },
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 Container(
+//                   margin: EdgeInsets.only(top: 15),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           padding: EdgeInsets.symmetric(
+//                               vertical: 20, horizontal: 30),
+//                         ),
+//                         onPressed: () {
+//                           getController.onClickLogin();
+//                         },
+//                         child: Text("Telefonu Doğrula"),
+//                       ),
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
