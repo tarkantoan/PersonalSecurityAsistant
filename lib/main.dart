4bf1160c6e5965d2579f2ddf68e43aa40be6c6d0
EmirSavukV2 <emirsvk55@gmail.com>
2022-02-12 20:24:09 +0300

    t

--- lib/main.dart
+++ lib/main.dart
@@ -19,24 +19,37 @@ Future<void> main() async {
   runApp(MaterialApp(
     theme: customThemeData,
     home: app.widget,
+    debugShowCheckedModeBanner: false,
   ));
 }
 
 ThemeData customThemeData = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: const MaterialColor(
-    0xff4438ca,
-    <int, Color>{
-      50: Color(0xffece7f9), //10%
-      100: Color(0xffcec5f0), //20%
-      200: Color(0xffad9ee7), //30%
-      300: Color(0xff8b76df), //40%
-      400: Color(0xff6f59d8), //50%
-      500: Color(0xff503dd0), //60%
-      600: Color(0xff4438ca), //70%
-      700: Color(0xff3130c1), //80%
-      800: Color(0xff172bba), //90%
-      900: Color(0xff001fae), //100%
-    },
-  ),
-);
+    colorScheme: const ColorScheme.dark(
+      background: Color(0xff121212),
+      surface: Color(0xff181818),
+      primary: Color(0xff4438ca),
+      secondary: Color(0xFFFFFFFF),
+      onBackground: Color(0xFFFFFFFF),
+      onSurface: Color(0xFFFFFFFF),
+      onPrimary: Color(0xff121212),
+      onSecondary: Color(0xff4438ca),
+    ),
+    brightness: Brightness.dark,
+    primarySwatch: const MaterialColor(
+      0xff4438ca,
+      <int, Color>{
+        50: Color(0xffece7f9), //10%
+        100: Color(0xffcec5f0), //20%
+        200: Color(0xffad9ee7), //30%
+        300: Color(0xff8b76df), //40%
+        400: Color(0xff6f59d8), //50%
+        500: Color(0xff503dd0), //60%
+        600: Color(0xff4438ca), //70%
+        700: Color(0xff3130c1), //80%
+        800: Color(0xff172bba), //90%
+        900: Color(0xff001fae), //100%
+      },
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color(0xff4438ca),
+    ));
